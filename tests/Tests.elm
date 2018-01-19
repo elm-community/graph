7cb6aad925e8682881c7ef28d2d7aa412fbcaa7f
Martin Janiczek <martin@janiczek.cz>
2018-01-19 17:27:32 +0100

    Add export to TGF

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,6 +2,7 @@ module Tests exposing (suite)
 
 import Test exposing (..)
 import Tests.Graph
+import Tests.Graph.TGF
 import Tests.Graph.Tree
 
 
@@ -10,4 +11,5 @@ suite =
     describe "elm-graph"
         [ Tests.Graph.all
         , Tests.Graph.Tree.all
+        , Tests.Graph.TGF.all
         ]
